d079e6eb6816ac65775429d984a5588d5e964b33
OmYadav <omydv3@gmail.com>
2026-02-13 10:44:10 +0530

    add vehicles slice for managing vehicle data in Redux store

---
+++ Frontend/src/store/services/VehiclesSlice.js
@@ -0,0 +1,20 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState ={
+    allVehicles: [],
+    Loading: false,
+}
+
+const vehiclesSlice = createSlice({
+    name: 'vehicles',
+    initialState,
+    reducers:{
+        setAllVehicles: (state, actions) => {
+            state.Loading = true
+            state.allVehicles = actions.payload;
+        }
+    }
+})
+export const { setAllVehicles } = vehiclesSlice.actions;
+export default vehiclesSlice.reducers;
+
--- Frontend/src/store/store.js
+++ Frontend/src/store/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import transactionsReducer from './services/TransactionsSlice';
+import vehiclesReducer from './services/VehiclesSlice';
 
 export const store = configureStore({
     reducer: {
         transactions: transactionsReducer,
+        vehicles: vehiclesReducer,
     },
 })
