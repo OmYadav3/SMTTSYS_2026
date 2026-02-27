b576008a310fc42431ca43aab3640a302d967959
OmYadav <omydv3@gmail.com>
2026-02-27 12:34:35 +0530

    fetch reports data by custom hook

--- Frontend/src/hooks/useFetchAllReports.js
+++ Frontend/src/hooks/useFetchAllReports.js
@@ -1,28 +1,40 @@
 import { REPORT_API_END_POINT } from "@/utils/constant";
 import { setAllReports } from "@/features/reports/reportsSlice";
-import axios from 'axios';
-import { useDispatch } from 'react-redux'
-import { useEffect } from "react";
+import axios from "axios";
+import { useDispatch } from "react-redux";
+import { useEffect, useState } from "react";
 
-const useFetchAllReports = () => {
-    const dispatch = useDispatch();
+const useFetchAllReports = (page = 1) => {
+  const dispatch = useDispatch();
 
-    useEffect(() => {
-        const fetchAllReports = async() => {
-            try {
-                // const endpoint = REPORT_API_END_POINT;
-                // console.log(endpoint)
-                const res = await axios.get('http://localhost:8000/api/v1/transactions?page=1')
-                console.log("RESPONSE OF REPORT: ", res.data)
-                if(res.data.success){
-                    dispatch(setAllReports(res.data))
-                }
-            } catch (error) {
-                console.log(error, "ERROR WHILE FETCHING THE REPORTS ")
-            }
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchAllReports = async () => {
+      try {
+        setLoading(true);
+        setError(null);
+
+        const res = await axios.get(
+          `${REPORT_API_END_POINT}?page=${page}`
+        );
+
+        if (res.data.success) {
+          dispatch(setAllReports(res.data.data));
         }
-        fetchAllReports();
-    },[dispatch])
-}
+      } catch (err) {
+        setError(err.message);
+        console.log("ERROR WHILE FETCHING REPORTS", err);
+      } finally {
+        setLoading(false);
+      }
+    };
 
-export default useFetchAllReports
+    fetchAllReports();
+  }, [dispatch, page]);
+
+  return { loading, error };
+};
+
+export default useFetchAllReports;
