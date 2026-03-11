f872a3f00855f5d111064232487519d773aa7f11
OmYadav <omydv3@gmail.com>
2026-03-11 13:14:16 +0530

    make a axios instance for helper

---
+++ Frontend/src/api/axiosInstance.js
@@ -0,0 +1,9 @@
+import { axios } from "axios";
+
+const axiosInstance = axios.create({
+    baseURL: 'http://localhost:8000/api/v1/reports',
+    timeout: 10000,
+
+});
+
+export default axiosInstance;
