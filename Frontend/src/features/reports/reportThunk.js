32955dc67911864bbc3a531cb3c9dd4e3c999e26
OmYadav <omydv3@gmail.com>
2026-03-11 15:39:17 +0530

    refactor files axiosinstance and report thunk.js

--- Frontend/src/features/reports/reportThunk.js
+++ Frontend/src/features/reports/reportThunk.js
@@ -2,7 +2,7 @@ import { createAsyncThunk } from "@reduxjs/toolkit";
 import { fetchReportAPI } from "./reportsAPI.js";
 
 export const fetchReports = createAsyncThunk(
-    "reports/fetchReports",
+    "report/fetchReports",
     async( filters ) => {
         const data = await fetchReportAPI(filters)
         return data;
