a162e747949bc791a85c68ce1693bbadb54e922d
OmYadav <omydv3@gmail.com>
2026-03-12 11:09:10 +0530

    refactor report service add safer limit to use min method

--- Server/src/routes/report.route.js
+++ Server/src/routes/report.route.js
@@ -3,6 +3,12 @@ import { getReports } from '../services/report.service.js'
 
 const router = Router();
 
-router.route('/').get(getReports)
+router.route('/get').get(getReports)
+
+
+router.route('/health').get((req, res) => {
+  res.send("Server running ✅");
+})
+
 
 export default router;
--- Server/src/services/report.service.js
+++ Server/src/services/report.service.js
@@ -1,7 +1,9 @@
 import * as reportRepo from '../repositories/report.repository.js'
 
 export const getReports = async(filters) => {
-    const limit = parseInt(filters.limit) || 50;
+
+    const limit = Math.min(parseInt(filters.limit) || 50, 500); /* USE MIN METHOD FOR NOT CRASH THE APP BY REQUESTING ROW LIKE 100000 */
+
     const data = await reportRepo.getReports({
         ...filters,
         limit
