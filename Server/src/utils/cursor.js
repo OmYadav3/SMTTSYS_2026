524fc226fbea46734f614d7e281ca33c14574c96
OmYadav <omydv3@gmail.com>
2026-03-20 13:25:45 +0530

    add secure cursor encoding/decoding with HMAC and TTL

---
+++ Server/src/utils/cursor.js
@@ -0,0 +1,52 @@
+import crypto from 'crypto';
+
+const SECRET = process.env.CURSOR_SECRET || 'topSecret';
+const TTL = 5 * 60 * 1000;     // TTl-Time to live
+
+/*-------------- ENCODED THE CURSOR_TOKEN--------------------- */
+
+export const encoderCursor = (payload) => {
+    const data = {...payload, exp: Date.now() + TTL};
+
+
+    // make a random string which is not readable
+    const base64 = Buffer.from(JSON.stringify(data)).toString("base64"); 
+
+    // make signature - if cursor change - signature break❌
+    const signature = crypto
+    .createHmac("sha256", SECRET)
+    .update(base64)
+    .digest("hex")
+
+    return `${base64}.${signature}`;
+};
+
+
+/*-------------- DECODED THE CURSOR_TOKEN--------------------- */
+
+export const decoderCursor = (cursor) => {
+    // Destructure the values - And split the value by dot
+    const [base64, signature] = cursor.split(".");
+
+
+    // Recreate the signature 
+    const expectedSignature = crypto
+    .createHmac("sha256", SECRET)
+    .update(base64)
+    .digest("hex")
+
+    // Verify the existing signature with new signature 
+    if(signature !== expectedSignature){
+        throw new Error("Invalid cursor")
+    }
+
+    // Decode the data 
+    const data = JSON.parse(
+        Buffer.from(base64, "base64").toString("utf-8")
+    );
+
+    // Check expiry of token 
+    if (Date.now() > data.exp) {
+        throw new Error("Cursor Expired")
+    }
+}
